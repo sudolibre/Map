37775c487b49ea41d60d5b7831584d386860090e
sudolibre <optout.breaker@gmail.com>
2016-12-13 18:27:05 -0500

    Initial Commit

---
+++ Map/Map.h
@@ -0,0 +1,19 @@
+//
+//  Map.h
+//  Map
+//
+//  Created by Jonathon Day on 12/13/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Map.
+FOUNDATION_EXPORT double MapVersionNumber;
+
+//! Project version string for Map.
+FOUNDATION_EXPORT const unsigned char MapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Map/PublicHeader.h>
+
+
