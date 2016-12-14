23aaf02150a93e2f02eb2112e96dd73bcb1ed5a8
sudolibre <optout.breaker@gmail.com>
2016-12-13 19:05:40 -0500

    need to push again. for some reason file is not showing in Map folder

--- Map.swift
+++ Map.swift
@@ -7,3 +7,130 @@
 //
 
 import Foundation
+
+enum Parity {
+    case even, odd
+}
+func int2Enum(intList: [Int]) -> [Parity] {
+    let parityList: [Parity] = intList.map { if $0 % 2 == 0 {
+        return Parity.even
+    } else {
+        return Parity.odd
+        }}
+    return parityList
+}
+
+
+
+// rubix
+//
+// Write a function which takes a list of integers and returns a list of their cubes
+//
+// input: [1, 2, 3, 4] output: [1, 8, 27, 64]
+
+func cuber(intList: [Int]) -> [Int] {
+    let cubedList = intList.map { $0*$0*$0 }
+    return cubedList
+}
+
+
+
+//dessert
+//
+//Write a method, dessert that accepts [[String: String]] and changes the values associated with keys in the following ways: if the key "ice cream" has a value, remove the key and associate the value with the new key "yogurt". If the key "toppings" has the value "jimmies", change that value to "sprinkles".
+//
+//[[“ice cream": "cherry”]] -> [[“yogurt": "cherry"]]
+//
+//[[“toppings": "jimmies", "ice cream": "cherry”]] -> [["toppings": "sprinkles", "yogurt": "cherry”]]
+//
+//[[“yogurt": "strawberry”]] -> [[“yogurt": "strawberry”]]
+
+
+func dessert(inputArray: [[String: String]]) -> [[String: String]] {
+    let newArrayOfDictionaries: [[String: String]] =  inputArray.map { dictionary in
+        var newDictionary = dictionary
+        if let kvpValue = newDictionary["ice cream"] {
+            print(kvpValue)
+            newDictionary.updateValue(kvpValue, forKey: "yogurt")
+            newDictionary.removeValue(forKey: "ice cream")
+        }
+        if newDictionary["toppings"] != nil {
+            newDictionary.updateValue("sprinkles", forKey: "toppings")
+        }
+        return newDictionary
+    }
+    return newArrayOfDictionaries
+}
+
+// rotZeroNoPunctuation
+//
+// Write a function which takes in a string and returns a “-“ separated list of numbers where each number represents the place of the given character in the english alphabet. input will consist only of lower case characters.
+//
+
+
+
+func alphabetPlace(word: String) -> String {
+    let alphabet = Array("abcdefghijklmnopqrstuvwxyz".characters)
+    return word.lowercased().characters.map { String(describing: alphabet.index(of: $0)!) }.joined(separator: "-")
+}
+
+//
+// rotZero
+//
+// Write a function which takes in a string and returns a “-“ separated list of numbers where each number represents the place of the given character in the english alphabet. input will consist only of lower case, upper case characters and punctuation. Punctuation (anything other than lower case characters) should be left alone. You are not expected to preserve case. 
+
+func alphabetPlaceWithPunctuation(word: String) -> String {
+    let alphabet = Array("abcdefghijklmnopqrstuvwxyz".characters)
+    return word.lowercased().characters.map {
+        if let index = alphabet.index(of: $0) {
+            return String(describing: index)
+        } else {
+            return String($0)
+        }}.joined(separator: "-")
+}
+
+
+
+//Write a function which takes in a string and returns a “-“ separated list of numbers where each number represents the place of the given character in the english alphabet with their number ‘rotated’ by 13. input will consist only of lower case, upper case characters and punctuation. Punctuation (anything other than lower case characters) should be left alone. You are not expected to preserve case. 
+func alphabetPlaceRot13(word: String) -> String {
+    let alphabet = Array("abcdefghijklmnopqrstuvwxyz".characters)
+    return word.lowercased().characters.map {
+        if let index = alphabet.index(of: $0) {
+            var rotIndex = index + 13
+            if rotIndex >= 26 {
+                rotIndex %= 26
+            }
+            return String(describing: rotIndex)
+        } else {
+            return String($0)
+        }}.joined(separator: "-")
+}
+
+
+
+
+
+//rot13 p 2
+//
+//Note that rot13Lossy preserves the 1 passed into it. This will lead to problems when converting back from the cypher. write a new version rot13 which will change each digit of any number into a corresponding upper case character. 
+
+func alphabetPlaceRot13P2(word: String) -> String {
+    let alphabet = Array("abcdefghijklmnopqrstuvwxyz".characters)
+    let numSet = Set("0123456789".characters)
+    return word.lowercased().characters.map {
+        if let index = alphabet.index(of: $0) {
+            var rotIndex = index + 13
+            if rotIndex >= 26 {
+                rotIndex %= 26
+            }
+            return String(describing: rotIndex)
+        } else {
+            if numSet.contains($0) {
+                let rotIndex = (Int(String($0))! - 1)
+                return String(alphabet[rotIndex]).uppercased()
+            } else {
+                return String($0)
+            }
+        }}.joined(separator: "-")
+}
+
--- MapTests/MapTests.swift
+++ MapTests/MapTests.swift
@@ -11,26 +11,83 @@ import XCTest
 
 class MapTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testInt2Enum() {
+    let result = int2Enum(intList: [1, 2, 3, 4]) //
+    let expected = [Parity.odd, Parity.even, Parity.odd, Parity.even]
+    XCTAssertEqual(result, expected)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testCuber() {
+    let result = cuber(intList: [1, 2, 3, 4])
+    let expected = [1, 8, 27, 64]
+    XCTAssertEqual(result, expected)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDessert() {
+        let dictionary1: [String: String] = ["ice cream": "cherry"]
+        let arrayOfDictionary1: [[String: String]] = [dictionary1]
+        
+        let result1 = dessert(inputArray: arrayOfDictionary1)
+        let expected1: [[String: String]] = [["yogurt": "cherry"]]
+        
+        let dictionary2: [String: String] = ["toppings": "jimmies", "ice cream": "cherry"]
+        let arrayOfDictionary2: [[String: String]] = [dictionary2]
+        
+        let result2 = dessert(inputArray: arrayOfDictionary2)
+        let expected2: [[String: String]] = [["toppings": "sprinkles", "yogurt": "cherry"]]
+
+        let dictionary3: [String: String] = ["yogurt": "strawberry"]
+        let arrayOfDictionary3: [[String: String]] = [dictionary3]
+        
+        let result3 = dessert(inputArray: arrayOfDictionary3)
+        let expected3: [[String: String]] = [["yogurt": "strawberry"]]
+        
+        XCTAssertEqual(result1[0], expected1[0])
+        XCTAssertEqual(result2[0], expected2[0])
+        XCTAssertEqual(result3[0], expected3[0])
+        
+        
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testAlphabetPlace() {
+        let result1 = alphabetPlace(word: "cake")
+        let expected1 = "2-0-10-4"
+        let result2 = alphabetPlace(word: "candy")
+        let expected2 = "2-0-13-3-24"
+        let result3 = alphabetPlace(word: "cAke")
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected1)
     }
     
+    func testAlphabetPlaceWithPunctuation() {
+        let result1 = alphabetPlaceWithPunctuation(word: "cake!")
+        let expected1 = "2-0-10-4-!"
+        
+        let result2 = alphabetPlaceWithPunctuation(word: "candy!")
+        let expected2 = "2-0-13-3-24-!"
+        
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+    }
+    
+    func testAlphabetPlaceRot13() {
+        let result1 = alphabetPlaceRot13(word: "Cake!")
+        let expected1 = "15-13-23-17-!"
+        
+        let result2 = alphabetPlaceRot13(word: "Candy!!1!")
+        let expected2 = "15-13-0-16-11-!-!-1-!"
+        
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+    }
+    
+    func testAlphabetPlaceRot13P2() {
+        let result = alphabetPlaceRot13P2(word: "I am 1337")
+        let expected = "21- -13-25- -A-C-C-G"
+        
+        XCTAssertEqual(result, expected)
+        
+    }   
+
 }
